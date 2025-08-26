d5a1d862ff24af87ee84c1767bd956a371d81d22
Justin Murray <justin@timescale.com>
2025-08-26 10:32:32 -0400

    Add a dynamic resource for memories

--- src/stdio.ts
+++ src/stdio.ts
@@ -2,9 +2,11 @@
 import { stdioServerFactory } from './shared/boilerplate/src/stdio.js';
 import { apiFactories } from './apis/index.js';
 import { context, serverInfo } from './serverInfo.js';
+import { additionalSetup } from './resources.js';
 
 stdioServerFactory({
   ...serverInfo,
   context,
   apiFactories,
+  additionalSetup,
 });
